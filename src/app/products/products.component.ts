be90ecd56333da6687c86ce2d6a6751a39967edb
iamhaitham <iamhaitham2011@gmail.com>
2022-05-20 18:17:00 +0300

    Use the injected products service to get all products on the initialization of the products component

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Product } from '../models/product.model';
 import { ProductsService } from './products.service';
 
 @Component({
@@ -7,12 +9,14 @@ import { ProductsService } from './products.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  products$: Observable<Product[]>;
 
   constructor(
     private productsService: ProductsService
   ) { }
 
   ngOnInit(): void {
+    this.products$ = this.productsService.getAllProducts$();
   }
 
 }
