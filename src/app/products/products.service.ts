d6fc50629afa601908aa989c4935a04bee6c1396
iamhaitham <iamhaitham2011@gmail.com>
2022-05-20 18:15:53 +0300

    Add getAllProducts method to the products service

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllProducts$(): Observable<Product[]>{
+    return this.http.get<Product[]>('https://fakestoreapi.com/products');
+  }
 }
