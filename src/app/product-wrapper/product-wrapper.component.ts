32aa72d5d02175c3530a6ab8ef7f7ba3c8aa41be
iamhaitham <iamhaitham2011@gmail.com>
2022-05-20 16:19:16 +0300

    Generated 'Product Wrapper' component

---
+++ src/app/product-wrapper/product-wrapper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-wrapper',
+  templateUrl: './product-wrapper.component.html',
+  styleUrls: ['./product-wrapper.component.css']
+})
+export class ProductWrapperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
