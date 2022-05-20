a446c12202144b0978fff07b750edea825231f90
iamhaitham <iamhaitham2011@gmail.com>
2022-05-20 21:16:16 +0300

    Inject and use CategoriesService in CategoriesComponent

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoriesService } from './categories.service';
 
 @Component({
   selector: 'categories',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
+  categories$: Observable<string[]>;
 
-  constructor() { }
+  constructor(
+    private categoriesService: CategoriesService
+  ) { }
 
   ngOnInit(): void {
+    this.categories$ = this.categoriesService.getAllCategories$();
   }
 
 }
