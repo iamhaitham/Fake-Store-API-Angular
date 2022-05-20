8b8a24e4eae2309e239998d58710f88e19c3d24c
iamhaitham <iamhaitham2011@gmail.com>
2022-05-20 22:11:44 +0300

    Add routes for ProductsComponent and ProductProfileComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductProfileComponent } from './product-profile/product-profile.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', component: ProductsComponent },
+  { path: 'products/:id', component: ProductProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
