030ca284903bf452720b8518dd5f9d6e3be10145
iamhaitham <iamhaitham2011@gmail.com>
2022-05-20 20:24:22 +0300

    Add getAllCategories method to CategoriesService

--- src/app/categories/categories.service.ts
+++ src/app/categories/categories.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from '../models/category.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor() { }
+  constructor(
+      private http: HttpClient
+  ) { }
+
+  getAllCategories$(): Observable<Category>{
+    return this.http.get<Category>('https://fakestoreapi.com/products/categories');
+  } 
+  
 }
