4b3b0c6bd491122e1e35f72b47c2d674ef93a92f
iamhaitham <iamhaitham2011@gmail.com>
2022-05-20 22:24:21 +0300

    Add route for page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductProfileComponent } from './product-profile/product-profile.component';
 import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   { path: 'products', component: ProductsComponent },
   { path: 'products/:id', component: ProductProfileComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
