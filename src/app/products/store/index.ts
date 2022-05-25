12996240b99cb69fb080bddf965a35b599f92c66
iamhaitham <iamhaitham2011@gmail.com>
2022-05-26 00:38:28 +0300

    Create an index.ts for Products Store

---
+++ src/app/products/store/index.ts
@@ -0,0 +1,4 @@
+export * from './actions/products.actions';
+export * from './reducers/products.reducer';
+export * from './effects/products.effects';
+export * from './selectors/products.selectors';
