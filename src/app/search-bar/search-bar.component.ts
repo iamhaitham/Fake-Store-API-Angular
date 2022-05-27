aa1f3afe59b83200794ded449afb6c94cb87fbbf
iamhaitham <iamhaitham2011@gmail.com>
2022-05-27 18:43:44 +0300

    Add an @Output for search-bar component

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,21 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subject, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent implements OnInit, OnDestroy {
   value: string;
+  searchTerm = new FormControl('');
+  destroyed$: Subject<boolean> = new Subject();
+  @Output() findProductByName$ = new EventEmitter<string>();
 
   constructor() { 
     this.value = '';
   }
 
   ngOnInit(): void {
+    this.searchTerm.valueChanges.pipe(
+      takeUntil(this.destroyed$),
+      tap((term: string) => this.findProductByName$.emit(term))
+    ).subscribe();
   }
 
   clearSearchBar(): void {
     this.value = '';
   }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
+  }
+
 }
