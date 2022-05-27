f249031d4aa6b06b65f7187ec2f5f5044a8a576e
iamhaitham <iamhaitham2011@gmail.com>
2022-05-27 19:56:19 +0300

    Create an index.ts file for Categories Store

---
+++ src/app/categories/store/index.ts
@@ -0,0 +1,4 @@
+export * from './actions/categories.actions';
+export * from './reducers/categories.reducer';
+export * from './effects/categories.effects';
+export * from './selectors/categories.selectors';
