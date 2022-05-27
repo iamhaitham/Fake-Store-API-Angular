5c21cd4dacb7c4d9585dd0404f55abad76dc29ee
iamhaitham <iamhaitham2011@gmail.com>
2022-05-27 19:52:49 +0300

    Generate and configure Categories Selectors

---
+++ src/app/categories/store/selectors/categories.selectors.ts
@@ -0,0 +1,12 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { categoriesFeatureKey, categoriesState } from '..';
+
+export const productsSelectFeature = createFeatureSelector<categoriesState>(categoriesFeatureKey);
+
+export class CategoriesSelectors {
+
+    static readonly categoryName = createSelector(
+        productsSelectFeature,
+      (state: categoriesState) => state.categoryName
+    );
+}
