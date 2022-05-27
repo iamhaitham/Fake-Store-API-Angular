8b2a68d9d155168dec13f892083f350e6d363ca8
iamhaitham <iamhaitham2011@gmail.com>
2022-05-27 22:16:04 +0300

    Make the search results to only be from the same category that is chosen

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { map, Observable } from 'rxjs';
+import { combineLatest, map, Observable, ReplaySubject, take, tap } from 'rxjs';
+import { CategoriesSelectors } from '../categories/store';
 import { Product } from '../models/product.model';
 import { ProductsActions, ProductsSelectors, productsState } from './store';
 
@@ -30,12 +31,27 @@ export class ProductsService {
   }
 
   findProductByName(productName: string): Observable<Product[]> {
-    return this.store.select(ProductsSelectors.data).pipe(
+    let productsByNameAndCategory$: ReplaySubject<Product[]> = new ReplaySubject<Product[]>();
+
+    let productsByName$ = this.store.select(ProductsSelectors.data).pipe(
       map(
         (products: Product[]) => 
         products.filter((p: Product) => (p.title.toLowerCase()).includes(productName.toLowerCase()))
       )
-    )
+    );
+
+    let selectedCategory$ = this.store.select(CategoriesSelectors.categoryName);
+
+    combineLatest([selectedCategory$, productsByName$]).pipe( // I want to filter the products in the ngrx store (observable 1) based on the categoryName that is also in the store (observable2), so I need the 'combineLatest' operator.
+      tap(([categoryName, products]) => productsByNameAndCategory$.next(
+        categoryName !== 'All' ? 
+          products.filter((product: Product) => product.category.toLowerCase().includes(categoryName))
+          : products
+      )
+      ),take(1) // I have used the 'take' operator to unsubscribe from the observable.
+    ).subscribe();
+
+    return productsByNameAndCategory$.asObservable();
   }
 
   loading$(): Observable<boolean> {
