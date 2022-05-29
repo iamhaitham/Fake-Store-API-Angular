d64f9604b485a9de3e1187d6d00b2f94610a5be0
iamhaitham <iamhaitham2011@gmail.com>
2022-05-29 17:18:19 +0300

    Configure ProductProfile component

--- src/app/product-profile/product-profile.component.ts
+++ src/app/product-profile/product-profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product.model';
+import { ProductProfileService } from './product-profile.service';
 
 @Component({
   selector: 'product-profile',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-profile.component.css']
 })
 export class ProductProfileComponent implements OnInit {
+  product$: Observable<Product>;
+  loading$: Observable<boolean>;
+  productId: number;
 
-  constructor() { }
+  constructor(
+    private productProfileService: ProductProfileService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.productId = +(this.activatedRoute.snapshot.params['id']);
+   }
 
   ngOnInit(): void {
+    this.productProfileService.loadProduct(this.productId);
+    this.loading$ = this.productProfileService.loading$();
+    this.product$ = this.productProfileService.currentProduct$();
   }
 
 }
