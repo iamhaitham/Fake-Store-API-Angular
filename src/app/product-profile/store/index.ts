fe44f00d9029a180e228b2fbb229597e6147a724
iamhaitham <iamhaitham2011@gmail.com>
2022-05-29 16:23:25 +0300

    Create index.ts for ProductProfile

---
+++ src/app/product-profile/store/index.ts
@@ -0,0 +1,4 @@
+export * from './actions/product-profile.actions';
+export * from './reducers/product-profile.reducer';
+export * from './effects/product-profile.effects';
+export * from './selectors/product-profile.selectors';
