886142a661051a3d8f53c875a6336a81cb3c33ae
iamhaitham <iamhaitham2011@gmail.com>
2022-06-03 02:02:42 +0300

    Register a path for LoginComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductProfileComponent } from './product-profile/product-profile.component';
 import { ProductsComponent } from './products/products.component';
@@ -7,6 +8,7 @@ import { ProductsComponent } from './products/products.component';
 const routes: Routes = [
   { path: 'products', component: ProductsComponent },
   { path: 'products/:id', component: ProductProfileComponent },
+  { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/products', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
